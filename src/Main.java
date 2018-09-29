92ce03c4d6a395bee8435fce3e240694b8d708c6
Arjun Padaliya <ap1170@wildcats.unh.edu>
2018-09-28 22:09:53 -0400

    Create empty GreenDash Client frame

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args)
 	{
-		
+		JFrame main = new JFrame();
+		main.setSize(800,600);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.setVisible(true);
 	}
 }
