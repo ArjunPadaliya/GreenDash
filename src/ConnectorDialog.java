4d286e89c403a8bf79157681b13d5a7a5a05c3a1
Arjun Padaliya <ap1170@wildcats.unh.edu>
2018-09-28 23:04:54 -0400

    Create ConnectorDialog frame with layout

--- src/ConnectorDialog.java
+++ src/ConnectorDialog.java
@@ -1,4 +1,74 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
 
-public class ConnectorDialog {
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class ConnectorDialog extends JDialog implements ActionListener {
+	
+	Properties dbProperties;
+	
+	JLabel hostLBL = new JLabel("Host");
+	JTextField host = new JTextField();
+	
+	JLabel portLBL = new JLabel("Port");
+	JTextField port = new JTextField();
+	
+	JLabel dbNameLBL = new JLabel("dbName");
+	JTextField dbName = new JTextField();
+	
+	JLabel userLBL = new JLabel("User");
+	JTextField user = new JTextField();
+	
+	JLabel passLBL = new JLabel("Password");
+	JTextField pass = new JTextField();
+	
+	JButton submitBTN = new JButton("submit");
+	JButton cancelBTN = new JButton("cancel");
+	
+	public ConnectorDialog(JFrame owner, String title, Properties p)
+	{
+		super(owner, title, true);
+		
+		setSize(400,400);
+		
+		JPanel dataPanel = new JPanel();
+		
+		dataPanel.setLayout(new GridLayout(5,2));
+		
+		dataPanel.add(hostLBL);
+		dataPanel.add(host);
+		
+		dataPanel.add(portLBL);
+		dataPanel.add(port);
+		
+		dataPanel.add(dbNameLBL);
+		dataPanel.add(dbName);
+		
+		dataPanel.add(userLBL);
+		dataPanel.add(user);
+		
+		dataPanel.add(passLBL);
+		dataPanel.add(pass);
+		
+		JPanel buttonPanel = new JPanel();
+		
+		buttonPanel.add(submitBTN);
+		buttonPanel.add(cancelBTN);
+		
+		add(dataPanel,BorderLayout.NORTH);
+		add(buttonPanel,BorderLayout.SOUTH);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import javax.swing.JFrame;
+import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args)
 	{
-		JFrame main = new JFrame();
+		JFrame main = new JFrame("GreenDash Client");
+		
+		Properties dbProperties = new Properties();
+		
+		ConnectorDialog inputDBInfo = new ConnectorDialog(main, "Database Info", dbProperties);
+		inputDBInfo.setVisible(true);
+		
+		
 		main.setSize(800,600);
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		main.setVisible(true);
